bb56a572b822e5ae9457a9633359273e4505247e
trebuhcsogait <schubert.tiago@gmail.com>
2020-06-02 19:09:04 -0500

    /

--- lesson6/js/windChill.js
+++ lesson6/js/windChill.js
@@ -1,12 +1,13 @@
 (function () {
     var H = document.getElementById('High').innerHTML;
     var w = document.getElementById('wind').innerHTML;
-    let windChill = 35.74 + (0.6215 * H) - (35.75 * (w ** 0.16)) + (0.4275 * H * (w ** 0.16));
+    let windChill = 35.74 + (0.6215 * H) - 
+    (35.75 * (w ** 0.16)) + (0.4275 * H * (w ** 0.16));
     
     if (H <= 50 && w > 3) {
        windChill = Math.round(windChill);
     } else {
-       windChill = "NA";
+       windChill = "N/A";
     }
     document.getElementById('windChill').innerHTML = windChill;
     }
